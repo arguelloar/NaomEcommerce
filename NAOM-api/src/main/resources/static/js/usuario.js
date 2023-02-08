d9b838b4b45830633e90ae325459eaafff3788d8
araa <arguello.ramosadrian@gmail.com>
2023-02-08 11:35:28 -0800

    Cancelar orden agregado

--- NAOM-api/src/main/resources/static/js/usuario.js
+++ NAOM-api/src/main/resources/static/js/usuario.js
@@ -6,9 +6,14 @@ let userAddress = document.getElementById("userAddress");
 let putUsuario = document.getElementById("putUsuario");
 let putNombre = document.getElementById("putNombre");
 let putTelefono = document.getElementById("putTelefono");
+let token = localStorage.getItem("token");
+console.log(token);
+let buttonCancelar = document.getElementsByClassName("cancel");
+
+//Bloque de try catch para obtener el token y obtener la informacion del usuario
 try {
-        const usuario = await getUser(localStorage.getItem("token")).then(response => response.json()); 
-        usuario.ordenes.forEach(orden => { tableOrders.innerHTML += addUserOrders(orden, usuario.direccion) })
+        const usuario = await getUser(token).then(response => response.json()); 
+        usuario.ordenes.forEach(orden => { tableOrders.insertAdjacentHTML("beforeend",addUserOrders(orden, usuario.direccion)) })
 
         userInfo.innerHTML =
                 `Nombre: ${usuario.nombre} ${usuario.apellido}<br><br>
@@ -21,9 +26,30 @@ try {
         window.location = "/"
 }
 
+Array.from(buttonCancelar).forEach(button => button.addEventListener("click", () => {
+        let id = button.id;
+        editOrden("CANCELADO",token,id).then(res => {
+                if(res.ok){
+                        location.reload();
+                }
+        })
+}))
 
 
 
+
+//Funcion de fetch para anadir ordenes.
+async function editOrden(estado, token,id) {
+        const response = await fetch(`https://naomecommerce-production.up.railway.app/api/orden/${id}?estado=${estado}`, {
+                method: "POST",
+                headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer: ${token.replaceAll('"', "")}`,
+                },
+        });
+        return response;
+}
+//Funcion para agregar en la tabla las ordenes
 function addUserOrders(orden, direccion) {
         return `
       <tr>
@@ -32,6 +58,7 @@ function addUserOrders(orden, direccion) {
       <td>${direccion}</td>
       <td>${orden.totalOrden}</td>
       <td>${orden.estado}</td>
+      <td><button class="cancel" id="${orden.id}">Cancelar</button></td>
       </tr>
       `;
 }
